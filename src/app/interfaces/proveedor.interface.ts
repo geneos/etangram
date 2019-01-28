d5bd871dfc1989d6308f3c20bc11a6d972ddc77a
LautaroSole <lautaro.sole@deuxit.com>
2019-01-28 12:10:28 -0300

    Cons Din:  - corregidas columnas al cambiar de objeto buscado A. Articulo, A. Prov:  - agregados botones de consulta  - falta metodos para otros datos en varios, faltan en la api

---
+++ src/app/interfaces/proveedor.interface.ts
@@ -0,0 +1,22 @@
+export interface Proveedor{
+  id: string;
+  codigo: number;
+  name: string;
+  cuit: number;
+  nombre_fantasia: string;
+  calle: string;
+  ciudad: number;
+  provincia: number;
+  cpostal: string;
+  pais: number;
+  id_categoria: string;
+  desc_categoria: string;
+  estado: string;
+  desc_estado: string;
+  id_localidad: number;
+  desc_localidad: string;
+  id_provincia: number;
+  desc_provincia: string;
+  id_civa: number;
+  civa: string;
+}

a88d40399b24625ca8ce8b812a0c4537446f8176
eayazzi <ezequiel.ayazzi@deuxit.com>
2019-02-15 10:40:43 -0300

    ord publicidad evidencias

--- src/app/components/pages/ordenes-publicidad/ordenes-publicidad.component.ts
+++ src/app/components/pages/ordenes-publicidad/ordenes-publicidad.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild,  ElementRef, Inject, Injectable } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { MatTable, MatSort, MatPaginator } from '@angular/material';
+import { MatTable, MatSort, MatPaginator, MatTableDataSource } from '@angular/material';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import { CompraService } from "../../../services/i2t/compra.service";
 import { CompraProveedor } from "../../../interfaces/compra.interface";
+import { OrdPublicidad } from "../../../interfaces/ord-publicidad.interface";
 import { ImpresionCompService } from "../../../services/i2t/impresion-comp.service";
 import { OrdPublicidadService } from "../../../services/i2t/ord-publicidad.service";
 import { ImpresionBase, informes } from "../../../interfaces/impresion.interface";
@@ -19,7 +21,14 @@ var auxProvData: any;
 @Component({
   selector: 'app-ordenes-publicidad',
   templateUrl: './ordenes-publicidad.component.html',
-  styleUrls: ['./ordenes-publicidad.component.css']
+  styleUrls: ['./ordenes-publicidad.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class OrdenesPublicidadComponent implements OnInit {
   loading: boolean = true;
@@ -32,7 +41,15 @@ export class OrdenesPublicidadComponent implements OnInit {
   urlBaseDatos: string;
   urlInforme: any;
   baseUrl: ImpresionBase[] = [];
-  //
+  //fin datos para impresion
+
+  datosOrdPublicidad: any;
+  aEjecutar: OrdPublicidad[] = [];
+  pendienteRendicion: OrdPublicidad[] = [];
+  enProcesoLiquidacion: OrdPublicidad[] = [];
+  expandedElement: OrdPublicidad | null;
+  columnsToDisplay  = ['numero', 'mes', 'medio', 'expediente', 'importe', 'acciones'];
+
   compraProveedor: CompraProveedor;
   proveedorData: any;
   id: any;
@@ -124,7 +141,7 @@ export class OrdenesPublicidadComponent implements OnInit {
       });
   }
 
-  print(nint: number) {
+  print(nint: string) {
     // window.open('http://devjasper.i2tsa.com.ar/Ejecutar_Reportes2.php?ruta_reporte=/E_Tangram/Reports/Compras/ET_ReferenciasContables&formato=PDF&param_param_nint=213334');
      this._ImpresionCompService.getBaseDatos( this.token )
      .subscribe ( dataP => {
@@ -133,13 +150,13 @@ export class OrdenesPublicidadComponent implements OnInit {
        this.baseUrl = this.proveedorData.dataset
        this.urlBaseDatos = this.baseUrl[0].jasperserver + this.baseUrl[0].app
        if(this.proveedorData.dataset.length>0){
-         this._ImpresionCompService.getInfromes('ETG_retencion_crd', this.token)
+         this._ImpresionCompService.getInfromes('ETG_ordenpublicidad', this.token)
          .subscribe ( dataP => {
            console.log(dataP)
            this.proveedorData = dataP;
            this.informes = this.proveedorData.dataset
            this.urlInforme = this.urlBaseDatos + this.informes[0].url
-           console.log(this.urlInforme)
+           console.log(this.urlInforme + nint)
            if(this.proveedorData.dataset.length>0){
              window.open(this.urlInforme + nint)
            }
@@ -149,15 +166,49 @@ export class OrdenesPublicidadComponent implements OnInit {
    }
 
    mostrarDatos(){
-     let jsbodydatos = {
+     let jsbodydatos10 = {
       "Id_Prov": this.id,
       "opc": 10
      }
-     let jsonbodydatos = JSON.stringify(jsbodydatos);
+     let jsonbodydatos10 = JSON.stringify(jsbodydatos10);
 
-     this._ordPublicidadService.postDatos(jsonbodydatos, this.token)
+     this._ordPublicidadService.postDatos(jsonbodydatos10, this.token)
       .subscribe ( dataD => {
         console.log(dataD);
+        this.datosOrdPublicidad = dataD;
+        this.aEjecutar = this.datosOrdPublicidad.dataset
+        // this.aEjecutar = new MatTableDataSource(this.datosOrdPublicidad);
+        // console.log(this.aEjecutar);
       })
+
+    let jsbodydatos20 = {
+      "Id_Prov": this.id,
+      "opc": 20
+     }
+     let jsonbodydatos20 = JSON.stringify(jsbodydatos20);
+
+     this._ordPublicidadService.postDatos(jsonbodydatos20, this.token)
+      .subscribe ( dataD => {
+        console.log(dataD);
+        this.datosOrdPublicidad = dataD;
+        this.pendienteRendicion = this.datosOrdPublicidad.dataset
+        // this.aEjecutar = new MatTableDataSource(this.datosOrdPublicidad);
+        // console.log(this.aEjecutar);
+      })
+
+    let jsbodydatos30 = {
+      "Id_Prov": this.id,
+      "opc": 30
+      }
+      let jsonbodydatos30 = JSON.stringify(jsbodydatos30);
+  
+      this._ordPublicidadService.postDatos(jsonbodydatos30, this.token)
+        .subscribe ( dataD => {
+          console.log(dataD);
+          this.datosOrdPublicidad = dataD;
+          this.enProcesoLiquidacion = this.datosOrdPublicidad.dataset
+          // this.aEjecutar = new MatTableDataSource(this.datosOrdPublicidad);
+          // console.log(this.aEjecutar);
+        }) 
    }
 }
---
+++ src/app/interfaces/ord-publicidad.interface.ts
@@ -0,0 +1,12 @@
+export interface OrdPublicidad {
+    num_operacion: string,
+    mes_pactado: string,
+    nombre_medio: string,
+    id_op_url: string,
+    importe: string,
+    estado: string,
+    descripcion: string,
+    id_proveedor: string,
+    expediente_id: string,
+    expediente_nombre: string
+}

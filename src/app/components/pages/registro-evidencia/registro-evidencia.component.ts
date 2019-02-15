a466e625a6fb0553c4bc3735a3c85dab0452486c
eayazzi <ezequiel.ayazzi@deuxit.com>
2019-02-15 13:46:29 -0300

    registro de evidencias

--- src/app/components/pages/registro-evidencia/registro-evidencia.component.ts
+++ src/app/components/pages/registro-evidencia/registro-evidencia.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormControlName } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { MatTable, MatHint, MatPaginator, MatSnackBar, MatTableDataSource} from '@angular/material';
+import { SESSION_STORAGE, StorageService } from 'angular-webstorage-service';
+import { NgxSmartModalService, NgxSmartModalComponent } from 'ngx-smart-modal';
 
 @Component({
   selector: 'app-registro-evidencia',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroEvidenciaComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+  loading: boolean = false;
+  nuevo: boolean = false;
+  suscripcionConsDin: Subscription;
+  itemDeConsulta: any;
+
+  constructor(public ngxSmartModalService: NgxSmartModalService,
+              public snackBar: MatSnackBar) { 
+              this.forma = new FormGroup({
+      'fecha': new FormControl(),
+      'descripcion': new FormControl(),
+      'image': new FormControl(),
+      'btn': new FormControl()
+    })
+  }
 
   ngOnInit() {
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message,"Cerrar", {
+      duration: 3000,
+    });
+  }
+
+  agregarEvidencia(){
+    this.nuevo = true;
+  }
+  guardarEvidencia(){
+    this.nuevo = false;
   }
 
 }

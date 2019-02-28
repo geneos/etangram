f5d6b912cbdeb5623690af6bea33ae8571429f32
emiliano.banchio <emiliano.banchio@deuxit.com>
2019-02-28 15:47:30 -0300

    sacados los datos hardcodeados de abm proveedores

--- src/app/components/pages/abm-proveedores/abm-proveedores.component.ts
+++ src/app/components/pages/abm-proveedores/abm-proveedores.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject, Injectable  } from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatTable, MatSort, MatPaginator, MatTableDataSource } from '@angular/material';
+import { ProveedoresService } from 'src/app/services/i2t/proveedores.service';
+import { Proveedor } from "../../../interfaces/proveedor.interface";
+import { SESSION_STORAGE, StorageService } from 'angular-webstorage-service';
 
-const PROVEEDORES:any[] = [
+// key that is used to access the data in local storage
+const TOKEN = '';
+
+/*const PROVEEDORES:any[] = [
   {'codigo':0,'razonSocial':'Deux IT SRL','cuit':'30-123456789-9','posicionFiscal':'IVA Responsable Inscripto'},
   {'codigo':1,'razonSocial':'Deux IT SRL','cuit':'30-123456789-9','posicionFiscal':'IVA Responsable Inscripto'},
   {'codigo':2,'razonSocial':'Lunix S.R.L.','cuit':'30-987654321-0','posicionFiscal':'IVA Responsable Inscripto'},
-];
+];*/
+
+@Injectable()
 
 @Component({
   selector: 'app-abm-proveedores',
@@ -14,19 +22,74 @@ const PROVEEDORES:any[] = [
   styleUrls: ['./abm-proveedores.component.css']
 })
 export class AbmProveedoresComponent implements OnInit {
-  constProveedores = new MatTableDataSource(PROVEEDORES);
+  constProveedores = new MatTableDataSource();
+  token: string;
+
+  proveedoresAll:Proveedor[];
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   selection = new SelectionModel(true, []);
+  loading: boolean;
+  prData: any;
 
-  constructor() { }
+  constructor(private _proveedoresService:ProveedoresService,
+    @Inject(SESSION_STORAGE) private storage: StorageService
+    ) { 
+      console.log(this.storage.get(TOKEN) || 'Local storage is empty');
+      this.token = this.storage.get(TOKEN);
+
+      this.loading = true;
+      this.buscarProveedores();
+  }
 
   ngOnInit() {
     this.paginator._intl.itemsPerPageLabel = 'Artículos por página:';
     this.constProveedores.sort = this.sort;
     this.constProveedores.paginator = this.paginator;
+  }
+
+  buscarProveedores(){
+    this._proveedoresService.getCProveedores( this.token )
+      .subscribe( dataRC => {
+        //console.log(dataRC);
+          this.prData = dataRC;
+          //auxProvData = this.proveedorData.dataset.length;
+          if(this.prData.returnset[0].RCode=="-6003"){
+            //token invalido
+            this.proveedoresAll = null;
+            console.log("token invalido");
+            //let jsbody = {"usuario":"usuario1","pass":"password1"}
+            //let jsonbody = JSON.stringify(jsbody);
+            //this._refContablesService.login(jsonbody)
+              //.subscribe( dataL => {
+              // console.log(dataL);
+              // this.loginData = dataL;
+              // this.token = this.loginData.dataset[0].jwt;
+              // this.buscarRefContable();
+              //});
+            } else {
+              if(this.prData.dataset.length>0){
+                this.proveedoresAll = this.prData.dataset;
+                console.log(this.proveedoresAll);
+
+                this.constProveedores = new MatTableDataSource(this.proveedoresAll);
+
+                this.constProveedores.sort = this.sort;
+                this.constProveedores.paginator = this.paginator;
+
+                //this.table.renderRows();
+                //this.paginator._intl.itemsPerPageLabel = 'Artículos por página:';
+
+                this.loading = false;
+
+              } else {
+                this.proveedoresAll = null;
+              }
+            }
+            //console.log(this.refContablesAll);
+      });
   }
 
   /** Whether the number of selected elements matches the total number of rows. */

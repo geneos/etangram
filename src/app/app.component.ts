1a329fc9a968c53201ad63a8a1ceaefef4822407
eayazzi <ezequiel.ayazzi@deuxit.com>
2019-04-03 09:08:18 -0300

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgxSmartModalService } from 'ngx-smart-modal';
+import { Router, Route, ActivatedRoute, NavigationEnd, NavigationError, NavigationStart, Event } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,37 @@ import { NgxSmartModalService } from 'ngx-smart-modal';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+idProv: any;
+portal: boolean = false;
+urlRuta: any;
+  constructor(public ngxSmartModalService: NgxSmartModalService,
+             // private http: Http,
+              private router: Router,
+              private route: ActivatedRoute){
+    router.events.subscribe( (event: Event) => {
+      if (event instanceof NavigationStart) {;
+        //if (this.router.url)
+      }
 
-  constructor(public ngxSmartModalService: NgxSmartModalService){}
+      if (event instanceof NavigationEnd) {
+          // Hide loading indicator
+          this.route.params.subscribe( parametros=>{
+            this.idProv = parametros['id'];
+             if (this.router.url.includes('/datos-proveedores/') == true){
+               this.portal = true; 
+            } else {
+              this.portal = false;
+             }
+          });
+      }
+
+      if (event instanceof NavigationError) {
+          // Hide loading indicator
+          // Present error to user
+          console.log(event.error);
+      }
+  });
+  }
   title = 'etangram';
   
 }

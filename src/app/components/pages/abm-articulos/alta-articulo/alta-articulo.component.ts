2956cf2e36061c44a47250be650a71e6968d6a9c
LautaroSole <lautaro.sole@deuxit.com>
2019-04-04 11:49:26 -0300

    Alta Articulo: adaptado para los cambios de la api por parte de i2t

--- src/app/components/pages/abm-articulos/alta-articulo/alta-articulo.component.ts
+++ src/app/components/pages/abm-articulos/alta-articulo/alta-articulo.component.ts
@@ -850,7 +850,7 @@ construirFoto(){
             this.forma.controls['gestionDespacho'].setValue(this.datosArticulos[0].gestiondespacho == '0' ? false : true);
             this.forma.controls['gestionLote'].setValue(this.datosArticulos[0].gestionlote == '0' ? false : true);
             this.forma.controls['gestionSerie'].setValue(this.datosArticulos[0].gestionserie == '0' ? false : true);
-            
+            console.log('Administra stock? ', this.datosArticulos[0].administrastock == '0' ? 'false' : 'true')
             this.forma.controls['administraStock'].setValue(this.datosArticulos[0].administrastock == '0' ? false : true);
             
             //todo descomentar cuando lo agreguen
@@ -1015,7 +1015,7 @@ construirFoto(){
               });
             }
             else{
-              this.forma.controls['administraStock'].setValue(false);
+              // this.forma.controls['administraStock'].setValue(false);
             }
           }
     });
@@ -1270,7 +1270,7 @@ construirFoto(){
         "precio_UCpa":this.forma.controls['precioUltCompra'].value,// 200,
         "fecha_UCpa": this.forma.controls['fechaUltCompra'].value,
         "idmoneda": this.forma.controls['idMonedaUltCompra'].value,// codigo de monedas → lista desplegable con tg01_monedas
-        "Cant_Op_cpa": (this.forma.controls['cantidadOptimaDeCompra'].value == null) ? '0' : this.truncar(this.forma.controls['cantidadOptimaDeCompra'].value),
+        "Cant_Op_cpa": (this.forma.controls['cantidadOptimaDeCompra'].value == null) ? '0' : this.redondearA2(this.forma.controls['cantidadOptimaDeCompra'].value),
         "PrecioU_vta": this.forma.controls['precioUltVenta'].value,
         "FechaU_vta": this.forma.controls['fechaUltVenta'].value,
         "idmoneda1": this.forma.controls['idMonedaUltVenta'].value,//, codigo de monedas-> lista desplegable con tg01_monedas
@@ -1292,10 +1292,9 @@ construirFoto(){
         /* "stockIdeal": this.forma.controls['stockIdeal'].value,
         "stockMax": this.forma.controls['stockMaximo'].value,
         "stockRepo":this.forma.controls['stockReposicion'].value, */
-        //todo cambiar
-        "stockIdeal": (this.forma.controls['administraStock'].value == true ? this.forma.controls['stockIdeal'].value : '0'),
-        "stockMax": (this.forma.controls['administraStock'].value == true ? this.forma.controls['stockMaximo'].value : '0'),
-        "stockRepo": (this.forma.controls['administraStock'].value == true ? this.forma.controls['stockReposicion'].value : '0'),
+        "stockIdeal": (this.forma.controls['administraStock'].value == true ? Number(this.forma.controls['stockIdeal'].value) : null),
+        "stockMax": (this.forma.controls['administraStock'].value == true ? Number(this.forma.controls['stockMaximo'].value) : null),
+        "stockRepo": (this.forma.controls['administraStock'].value == true ? Number(this.forma.controls['stockReposicion'].value) : null),
         //unidad de medida
         "dimensiones": this.forma.controls['Dimensiones'].value,
         "pesable": this.forma.controls['Pesable'].value,
@@ -1308,10 +1307,10 @@ construirFoto(){
         "idUM5": this.forma.controls['umVentas'].value,
         "IdUM6": this.forma.controls['umPVenta'].value,
         //todo cambiar si lo cambian en la api, en else también
-        "largo": (this.forma.controls['largo'].value == null) ? '0' : this.truncar(this.forma.controls['largo'].value),
-        "ancho":(this.forma.controls['ancho'].value == null) ? '0' : this.truncar(this.forma.controls['ancho'].value),
-        "profundo":(this.forma.controls['profundidad'].value == null) ? '0' : this.truncar(this.forma.controls['profundidad'].value),
-        "m3": (this.forma.controls['m3'].value == null) ? '0' : this.truncar(this.forma.controls['m3'].value)
+        "largo": (this.forma.controls['largo'].value == null) ? '0' : this.redondearA2(this.forma.controls['largo'].value),
+        "ancho":(this.forma.controls['ancho'].value == null) ? '0' : this.redondearA2(this.forma.controls['ancho'].value),
+        "profundo":(this.forma.controls['profundidad'].value == null) ? '0' : this.redondearA2(this.forma.controls['profundidad'].value),
+        "m3": (this.forma.controls['m3'].value == null) ? '0' : this.redondearA2(this.forma.controls['m3'].value)
       }
     }
     else{
@@ -1349,7 +1348,7 @@ construirFoto(){
         // "idmoneda": this.forma.controls['idMonedaUltCompra'].value,// codigo de monedas → lista desplegable con tg01_monedas
         "idmoneda": this.forma.controls['idMonedaUltCompra'].value,// codigo de monedas → lista desplegable con tg01_monedas
         // "Cant_Op_cpa": this.forma.controls['cantidadOptimaDeCompra'].value,
-        "Cant_Op_cpa": (this.forma.controls['cantidadOptimaDeCompra'].value == null) ? '0' : this.truncar(this.forma.controls['cantidadOptimaDeCompra'].value),
+        "Cant_Op_cpa": (this.forma.controls['cantidadOptimaDeCompra'].value == null) ? '0' : this.redondearA2(this.forma.controls['cantidadOptimaDeCompra'].value),
         "PrecioU_vta": PrecioU_vta,//this.forma.controls['precioUltVenta'].value,
         "FechaU_vta": this.forma.controls['fechaUltVenta'].value,
         // "idmoneda1": this.forma.controls['idMonedaUltVenta'].value,//, codigo de monedas-> lista desplegable con tg01_monedas
@@ -1372,10 +1371,9 @@ construirFoto(){
         /* "stockIdeal": this.forma.controls['stockIdeal'].value,
         "stockMax": this.forma.controls['stockMaximo'].value,
         "stockRepo":this.forma.controls['stockReposicion'].value, */
-        //todo cambiar
-        "stockIdeal": (this.forma.controls['administraStock'].value == true ? this.forma.controls['stockIdeal'].value : '0'),
-        "stockMax": (this.forma.controls['administraStock'].value == true ? this.forma.controls['stockMaximo'].value : '0'),
-        "stockRepo": (this.forma.controls['administraStock'].value == true ? this.forma.controls['stockReposicion'].value : '0'),
+        "stockIdeal": (this.forma.controls['administraStock'].value == true ? Number(this.forma.controls['stockIdeal'].value) : null),
+        "stockMax": (this.forma.controls['administraStock'].value == true ? Number(this.forma.controls['stockMaximo'].value) : null),
+        "stockRepo": (this.forma.controls['administraStock'].value == true ? Number(this.forma.controls['stockReposicion'].value) : null),
         //unidad de medida
         "dimensiones": this.forma.controls['Dimensiones'].value,
         "pesable": this.forma.controls['Pesable'].value,
@@ -1387,10 +1385,10 @@ construirFoto(){
         "idUM4": this.forma.controls['umPCompra'].value,
         "idUM5": this.forma.controls['umVentas'].value,
         "IdUM6": this.forma.controls['umPVenta'].value,
-        "largo": (this.forma.controls['largo'].value == null) ? '0' : this.truncar(this.forma.controls['largo'].value),
-        "ancho":(this.forma.controls['ancho'].value == null) ? '0' : this.truncar(this.forma.controls['ancho'].value),
-        "profundo":(this.forma.controls['profundidad'].value == null) ? '0' : this.truncar(this.forma.controls['profundidad'].value),
-        "m3": (this.forma.controls['m3'].value == null) ? '0' : this.truncar(this.forma.controls['m3'].value)
+        "largo": (this.forma.controls['largo'].value == null) ? '0' : this.redondearA2(this.forma.controls['largo'].value),
+        "ancho":(this.forma.controls['ancho'].value == null) ? '0' : this.redondearA2(this.forma.controls['ancho'].value),
+        "profundo":(this.forma.controls['profundidad'].value == null) ? '0' : this.redondearA2(this.forma.controls['profundidad'].value),
+        "m3": (this.forma.controls['m3'].value == null) ? '0' : this.redondearA2(this.forma.controls['m3'].value)
       }  
     }
     console.log('stringifeando esto: ', jsbody)
